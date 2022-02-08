ba4ae5708609688a0a29edf2d236351dc29505da
mohkha <mohkha@umich.edu>
2022-02-08 01:30:02 -0500

    Added comments endpoint

--- insta485/api/__init__.py
+++ insta485/api/__init__.py
@@ -4,5 +4,6 @@ from insta485.api.posts import rest_get_post
 from insta485.api.index import rest_get_index
 from insta485.api.likes import rest_create_like
 from insta485.api.likes import rest_delete_like
+from insta485.api.comments import rest_create_comment
 from insta485.api.helpers import InvalidUsage
 from insta485.api.helpers import handle_invalid_usage
